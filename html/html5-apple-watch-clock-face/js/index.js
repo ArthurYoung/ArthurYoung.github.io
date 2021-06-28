9e948ea359280fbabd5802fcd32d1479f06453b6
pan <1360042077@qq.com>
2021-06-29 00:32:22 +0800

    fix

--- html/html5-apple-watch-clock-face/js/index.js
+++ html/html5-apple-watch-clock-face/js/index.js
@@ -43,19 +43,31 @@ function utilityClock(container) {
         element.style.transform = element.style.webkitTransform = 'rotate(' + (second * 6) + 'deg)'
     }
     var animate = function () {
-        var now = new Date()
-        var time = now.getHours() * 3600 +
+        /*var time = now.getHours() * 3600 +
             now.getMinutes() * 60 +
             now.getSeconds() * 1 +
-            now.getMilliseconds() / 1000
+            1000*/
+
+        // var time =new Date().getTime() % (1000*60*60*12) /1000 +60*60*8;
+        var time = (new Date().getTime() % 43200000 / 1000 + 28800).toFixed(0);
+        // var time =(new Date().getTime() % 43200000 /1000 +28800);
+
+
+        // now.getMilliseconds() / 1000
+
         rotate(secondElement, time)
         rotate(minuteElement, time / 60)
         rotate(hourElement, time / 60 / 12)
-        requestAnimationFrame(animate)
+
+        // requestAnimationFrame(animate)
+
     }
     for (var i = 1; i <= 60; i++) minute(i)
     for (var i = 1; i <= 12; i++) hour(i)
-    animate()
+    animate();
+    setInterval(function () {
+        animate();
+    }, 100)
 }
 
 function autoResize(element, nativeSize) {
@@ -66,6 +78,43 @@ function autoResize(element, nativeSize) {
     update()
     window.addEventListener('resize', update)
 }
+
+function createElement(type, attrArray, evtListener, html) {
+    let node = document.createElement(type);
+
+    for (let attr in attrArray) if (attrArray.hasOwnProperty(attr)) {
+        node.setAttribute(attr, attrArray[attr]);
+    }
+
+    if (evtListener) {
+        let a = evtListener.split(' ');
+        node.addEventListener(a[0], eval(a[1]), eval(a[2]));
+    }
+
+    if (html) node.innerHTML = html;
+
+    return node;
+}
+
+setInterval(function () {
+    var week = ['周日', '周一', '周二', '周三', '周四', '周五', '周六'];
+    let date = new Date();
+    let w = '<span class="dateRi">' + (date.getUTCDate() + 1) + '</span>' + week[date.getDay()];
+    let divDic = createElement('div', {
+        id: 'divDic'
+    }, null, w);
+    // divDic.addEventListener('mousedown', dragHandler, false);
+    let hm = document.querySelector('.week');
+    let we = document.querySelector("#divDic");
+    if (we == null && hm != null) {
+        hm.appendChild(divDic);
+    } else {
+        if (we.innerHTML != w) {
+            we.innerHTML = w;
+        }
+    }
+
+}, 50)
 
 if (document.body.offsetWidth > 1500) {
     document.querySelector('.fill').style.zoom = '80%'
